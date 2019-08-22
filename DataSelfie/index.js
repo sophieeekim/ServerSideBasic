25b0d3367ea0004532b40b46ba7d7b8d95d8bab5
sophie-kim <keun195@gmail.com>
2019-08-21 23:02:20 -0700

    client server communication

--- DataSelfie/index.js
+++ DataSelfie/index.js
@@ -3,3 +3,16 @@ const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('listening at port 3000'));
 app.use(express.static('public'));  //serves up any files in public folder
+app.use(express.json({limit:'1mb'})); //parse json data from client side
+ 
+//app post method
+app.post('/api', (request, response) => { // receiving data from client side
+    console.log(request.body);
+    const data = request.body;
+
+    response.json({ // sending to client
+        status: 'success',
+        latitude: data.lat,
+        longitude: data.lng
+    });
+});
