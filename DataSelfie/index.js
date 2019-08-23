9d0d5fdbcd3a33578c52ec788bdbfdd1ac6c9cb3
sophie-kim <keun195@gmail.com>
2019-08-22 22:52:51 -0700

    review request response

--- DataSelfie/index.js
+++ DataSelfie/index.js
@@ -9,10 +9,26 @@ app.use(express.json({limit:'1mb'})); //parse json data from client side
 app.post('/api', (request, response) => { // receiving data from client side
     console.log(request.body);
     const data = request.body;
-
-    response.json({ // sending to client
-        status: 'success',
-        latitude: data.lat,
-        longitude: data.lng
-    });
+    const dataResponse = [];
+    
+    data.forEach (element => {
+        const dataElement = {
+            status: 'success',
+            latitude: element.lat,
+            longitude: element.lng,
+            timestamp: element.timestamp
+        };
+        dataResponse.push(dataElement);
+    })
+    
+    response.json(dataResponse);
+    // data.forEach(element => {
+    //     response.json({ // sending to client
+    //         status: 'success',
+    //         latitude: element.lat,
+    //         longitude: element.lng,
+    //         timestamp: element.timestamp
+    //     });
+    // });
+    
 });
